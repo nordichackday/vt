4df37991db23b226c800929a73ee84f7deb8318f
Ronnie Jespersen <roje@dr.dk>
2015-05-28 17:32:13 +0200

    Manually forcing dev mode

--- application/web/index.php
+++ application/web/index.php
@@ -4,7 +4,7 @@ use VT\Application;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$env = getenv('SYMFONY_ENV') ?: 'prod';
+$env = getenv('SYMFONY_ENV') ?: 'dev';
 
 $application = new Application(__DIR__.'/../', $env, in_array($env, ['dev']));
 $application->run();
