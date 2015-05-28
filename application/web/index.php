822c2c7cac296b32af5b3ac0d30aeb8ae745272a
Ronnie Jespersen <roje@dr.dk>
2015-05-28 15:07:35 +0200

    Initializing silex

--- application/web/index.php
+++ application/web/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello world";
+use VT\Application;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$env = getenv('SYMFONY_ENV') ?: 'prod';
+
+$application = new Application(__DIR__.'/../..', $env, in_array($env, ['dev']));
+$application->run();
